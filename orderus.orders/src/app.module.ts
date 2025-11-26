2cc2b2f5475fb600a26f5abe8cf5bd99eb620be0
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-11-26 14:24:32 +0300

    add  items module with CRUD functionality and related DTOs

--- orderus.orders/src/app.module.ts
+++ orderus.orders/src/app.module.ts
@@ -1,15 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { OrdersModule } from './modules/orders/orders.module';
 import { DaprModule } from './dapr/dapr.module';
 import { CommonModule } from './common/common.module';
-import { OrdersResolver } from './modules/orders/orders.resolver';
 import { OrdersModule } from './modules/orders/orders.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSource, dataSourceOptions } from './db/data-soucre';
+import { ItemsModule } from './modules/items/items.module';
 
 @Module({
-  imports: [OrdersModule, CommonModule, DaprModule],
+  imports: [
+    OrdersModule,
+    CommonModule, 
+    DaprModule,
+  
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        ...dataSourceOptions,
+      }),
+      async dataSourceFactory(options) {
+        if (!options) {
+          throw new Error('Invalid options passed');
+        }
+        return dataSource;
+      },
+    }),
+  
+    ItemsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, OrdersResolver],
+  providers: [AppService],
 })
 export class AppModule {}
